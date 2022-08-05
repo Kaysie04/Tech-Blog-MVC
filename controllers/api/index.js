64f53e9e5f5fcf88c28363dfbaedea57c803a976
Kaysie <kaysielandersonwork@gmail.com>
2022-08-05 06:49:38 -0500

    added comments to api index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,18 @@
+
+// needed to connect to the server
 const router = require('express').Router();
 
+
+// attains routes from respective files
 const userRoutes =  require('./user-routes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes')
 
+// route path that the api will use for endpoints
+// the route path will read api/users api/posts api/comments
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+
 
 module.exports = router;
