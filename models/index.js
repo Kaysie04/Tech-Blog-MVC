f3ca806613422acf2dd3f34c12cfd5ab0051e1c3
Kaysie <kaysielandersonwork@gmail.com>
2022-08-05 06:24:40 -0500

    created associations

--- models/index.js
+++ models/index.js
@@ -4,16 +4,39 @@ const User = require('./User');
 const Comment = require('./Comment');
 
 
+// create associations
 
 
+// a user can create many posts, 
+//those posts belong to the user,
+// the post is assoicated with the user through the user_id
 
+User.hasMany(Post,{
+    foreignKey: 'user_id'
+});
 
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
 
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
 
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
 
-
-
-
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
 
 module.exports = { User, Post, Comment };
